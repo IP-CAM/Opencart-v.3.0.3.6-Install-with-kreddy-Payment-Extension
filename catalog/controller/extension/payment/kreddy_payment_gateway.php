1e95604513f0116dbc61c5bb80f51d2832c1f7bb
IvicaGj <ivizagj@gmail.com>
2020-10-16 21:02:00 +0200

    Logic with succesful loan application implemented. Email notification after order completion remains, and previewing the success page.

--- catalog/controller/extension/payment/kreddy_payment_gateway.php
+++ catalog/controller/extension/payment/kreddy_payment_gateway.php
@@ -4,10 +4,9 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
     /**
      * Accountable for calculating and parsing the necessary data using the helper methods for installment calculation using the Kreddy API
      */
-    public function index() {
-        $this->load->model('checkout/order');
-
-        $order = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+    public function index() 
+    {
+        $order = $this->getOrder($this->session->data['order_id']);
         
 		$total = trim($this->currency->format($order['total'], $this->config->get('config_currency')), ',денари');
         
@@ -55,26 +54,51 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
     /**
      * Gets fired after payment form confirmation
      */
-    public function confirm() {
+    public function confirm() 
+    {
         $formData = $_POST ? $_POST : null;
         $parsedData = '';
-        $clientID = '';
-        $clientHasActiveLoan = 0;
+        $clientId = '';
 
+        // parse necessary data into array
         if ($formData) {
             $parsedData = $this->parseFormData($formData);
         }
         
+        //check if client exists
         if ($parsedData) {
             if ($parsedData['agreement_checked'] == 1 && $parsedData['kreddy_form']['client_embg'] != '') {
                 $userCheck = $this->findKreddyUserByDocNum($parsedData['kreddy_form']['doc_type'], $parsedData['kreddy_form']['doc_number']);
             }
         }
-        print_r($userCheck);die();
+        
+        //create client if it doesnt exist
+        if (!$userCheck) {
+            $clientId = $this->createKreddyClient($parsedData['client'], $parsedData['kreddy_form']);
+        } else {
+            $clientId = $userCheck['id'];
+        }
+
+        $app = [];
+        //create application
+        if ($clientId) {
+            $app = $this->createLoanApplication($clientId, $parsedData['client']['ip_address'], $parsedData['loan_info']);
+        }
+
+        //failed order status code
+        $orderStatusCode = 10;
+
+        //loan application is good
+        if (isset($app['code']) && $app['code'] == 'OK') {
+            $orderStatusCode = 1;
+            
+            //send email here
+        }
+
 		if ($this->session->data['payment_method']['code'] == 'kreddy_payment_gateway') {
 			$this->load->model('checkout/order');
 
-			$this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('payment_kreddy_payment_gateway_order_status_id'));
+			$this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $orderStatusCode);
 		}
     }
     
@@ -113,7 +137,8 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
     /**
      * Parse cookies into string or array
      */
-    public function parseCookies($authorize, $array = false) {
+    public function parseCookies($authorize, $array = false)
+    {
         $authInfo = explode('GMT', $authorize);
         
         $cookiesObj = json_decode(end($authInfo));
@@ -384,7 +409,8 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
     /**
      * Parses the received form data into an array
      */
-    public function parseFormData($formData) {
+    public function parseFormData($formData) : array 
+    {
         //client billing data
         $clientFirstName = isset($formData['clientFirstName']) ? $formData['clientFirstName'] : '';
         $clientLastName = isset($formData['clientLastName']) ? $formData['clientLastName'] : '';
@@ -444,7 +470,8 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
     /**
      * Retrieve order by id
      */
-    public function getOrder($orderNum) {
+    public function getOrder($orderNum) 
+    {
         $this->load->model('checkout/order');
 
         $order = $this->model_checkout_order->getOrder($orderNum);
@@ -455,7 +482,8 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
     /**
      * Searches for possible existing user in the kreddy records
      */
-    public function findKreddyUserByDocNum($docType, $docNumber) {
+    public function findKreddyUserByDocNum($docType, $docNumber) 
+    {
         $docTypeID = '';
 
         if ($docType == 0){
@@ -494,5 +522,196 @@ class ControllerExtensionPaymentKreddyPaymentGateway extends Controller
         }
 
         return false;
+    }
+
+    /**
+     * Creates new Kreddy client 
+     */
+    public function createKreddyClient($clientData, $formData) 
+    {
+        // construct body of request
+        $body = [
+            'clientCreateRequest' => [
+                'RequestObject' => [
+                    'ClientStatusID' => '6ec0f766-d7f5-49b9-b36b-e63ea94de72a',
+                    'CommercialBrandID' => '02775ed0-ca6e-4284-a391-8c1b22eef008',
+                    'Firstname' => $clientData['first_name'],
+                    'Lastname' => $clientData['last_name'],
+                    'RegDocuments' => [
+                        0 => [
+                            'DocNumber' => $formData['client_embg'],
+                            'DocTypeID' => 'BDC463D4-FE42-406A-A3D8-C87320E3536C',
+                            'DocTypeName' => 'Jmbg',
+                        ],
+                        1 => [
+                            'DocNumber' => $formData['doc_number'],
+                            'DocTypeID' => '1188b317-4229-4c67-8d20-19048e279aac',
+                            'DocTypeName' => $formData['doc_type'] == 0 ? 'ID card' : 'Passport',
+                        ],
+                    ],
+                    'Communications' => [
+                        0 => [
+                            'CommTypeID' => 'd4a2dc80-30ca-df11-9b2a-001d60e938c6',
+                            'CommTypeName' => 'Phone',
+                            'CommValue' => $clientData['phone'],
+                            'Confirmed' => null,
+                        ],
+                        1 => [
+                            'CommTypeID' => 'ee1c85c3-cfcb-df11-9b2a-001d60e938c6',
+                            'CommTypeName' => 'Email',
+                            'CommValue' => $clientData['email'],
+                            'Confirmed' => null,
+                        ],
+                    ],
+                ]
+            ]
+        ];
+
+        //api call headers
+        $headers = [
+            'Content-Type: application/json'
+        ];
+
+        //get cookies in string format
+        $cookies = $this->parseCookies($this->authorize());
+
+        //api call url
+        $url = 'https://88.85.110.253/0/rest/FinstarClientAPI/clients';
+
+        $curl = curl_init($url);
+        
+        //api call options
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10000);
+        curl_setopt($curl, CURLOPT_COOKIE, $cookies);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_VERBOSE, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        //send call
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        //decode response
+        $response = json_decode($result, true);
+
+        //if client was created return his id : return false
+        if ($response['ClientCreateResult']['Code'] == 'OK') {
+            return $response['ClientCreateResult']['Id'];
+        } 
+
+        return false;
+    }
+
+    /**
+     * Creates loan application via the API
+     *  */
+    public function createLoanApplication($clientId, $clientIp, $loanData) 
+    {
+        $cookies = $this->parseCookies($this->authorize());
+
+        $offerInfo = $this->offerInfo($cookies);
+        
+        $offer = $this->parseOffer($offerInfo['GetRelatedOffersListResult']['ResponseObject']['offers']);
+        
+        $productId = $offer['productId'];
+
+        $cartTotal = rtrim($loanData['amount_to_pay'], ' мкд') - rtrim($loanData['apr_amount'], ' мкд');
+        
+        //body of API request
+        $body = [
+            'LoanApplicationCreateRequest' => [
+                'RequestObject' => [
+                    'ClientID' => $clientId,
+                    'DisbursmentMethodID' => '25ab9503-6c8c-4ae0-802f-f76c0be9b610',
+                    'LoanApplicationStatusID' => '1E8C32B6-1CC9-43E6-B45A-17AC51945208',
+                    'Parameters' => [
+                        0 => [
+                            'LoanApplicationParameterID' => 'E477D926-A5B9-43A5-97FF-62B9ADC711D5',
+                            'LoanApplicationParameterValue' => $cartTotal,
+                        ],
+                        1 => [
+                            'LoanApplicationParameterID' => 'E9FC717B-AF4C-4C4C-85CD-C4FA09A944D8',
+                            'LoanApplicationParameterValue' => $loanData['due_date'],
+                        ],
+                        2 => [
+                            'LoanApplicationParameterID' => '223CB1CB-6BAA-4854-AB0D-03FBD620BA25',
+                            'LoanApplicationParameterValue' => '0.00',
+                        ],
+                        3 => [
+                            'LoanApplicationParameterID' => '42E7A51C-F936-4078-BA7F-6E24552E3280',
+                            'LoanApplicationParameterValue' =>  rtrim($loanData['apr_amount'], ' мкд'),
+                        ],
+                        4 => [
+                            'LoanApplicationParameterID' => '5E88395F-AA88-43F8-957E-A8A3A30F2EAE',
+                            'LoanApplicationParameterValue' => rtrim($loanData['amount_to_pay'], ' мкд'),
+                        ],
+                        5 => [
+                            'LoanApplicationParameterID' => 'F31DD9A9-BD38-4D01-BACD-5FEC11E5FB2A',
+                            'LoanApplicationParameterValue' => rtrim($loanData['apr'], ' %'),
+                        ],
+                        6 => [
+                            'LoanApplicationParameterID' => '510BA101-5A11-4DA8-8846-150F9AD66F7D',
+                            'LoanApplicationParameterValue' => '0.00',
+                        ],
+                        7 => [
+                            'LoanApplicationParameterID' => 'C644C489-D8C3-47DA-86EF-A80DA89DD747',
+                            'LoanApplicationParameterValue' => $loanData['installments'],
+                        ],
+                        8 => [
+                            'LoanApplicationParameterID' => 'C1B7C74E-19C2-4AFA-BA3B-009B9A3103BA',
+                            'LoanApplicationParameterValue' => rtrim($loanData['fee'], ' мкд'),
+                        ],
+                    ],
+                    'ProductID' => $productId,
+                    'SigningMethodID' => '8801dbed-0af8-4141-9247-77d07ef668dc',
+                    'CreationIP' => $clientIp,
+                    'MarkAgree' => true,
+                    'UserAgree' => true,
+                ]
+            ]
+        ];
+
+        //API call headers
+        $headers = [
+            'Content-Type: application/json'
+        ];
+        
+        //url of API call
+        $url = 'https://88.85.110.253/0/rest/FinstarLoanApplicationAPI/loan-applications/';
+
+        $curl = curl_init($url);
+        
+        //api call options
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($curl, CURLOPT_TIMEOUT, 50000);
+        curl_setopt($curl, CURLOPT_COOKIE, $cookies);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_VERBOSE, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+
+        //send call
+        $result = curl_exec($curl);
+        curl_close($curl);
+        
+        //decode response
+        $response = json_decode($result, true);
+        
+        $applicationResult = [];
+
+        //set application result data if response is ok
+        if (isset($response['LoanApplicationCreateResult']) && $response['LoanApplicationCreateResult']['Code'] == "OK") {
+            $applicationResult['code'] = $response['LoanApplicationCreateResult']['Code'];
+            $applicationResult['application_id'] = $response['LoanApplicationCreateResult']['Id'];
+        }
+
+        return $applicationResult;
     }
 }
